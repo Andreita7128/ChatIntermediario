c86bb3c5d1d9f3733424f8929a5e161c6ab62b48
Andrea Torrente <andrea.torrente1@u.icesi.edu.co>
2022-05-24 22:44:36 -0500

    finish with little error

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 let screens = [];
-let actualScreen = 0;
+let currentScreen = 0;
 
 function preload() {
   screens[0] = loadImage('./assets/intermediario1.png');
@@ -17,45 +17,171 @@ function setup() {
 function draw() {
   background(220);
   showScreen();
-  
+  loadingRequest();
 }
 
 function mousePressed() {
   menu();
+  flowRequest();
 }
 
 function showScreen() {
-  image(screens[actualScreen],0 ,0);
+  image(screens[currentScreen], 0, 0);
 }
 
 function screenChanger(screen) {
-  actualScreen = screen;
+  currentScreen = screen;
 }
 
-function button ({xButton, yButton, wButton, hButton, screen}) {
+function button({
+  xButton,
+  yButton,
+  wButton,
+  hButton,
+  screen
+}) {
   const widthB = xButton + wButton;
   const heightB = yButton + hButton;
   if (xButton < mouseX && mouseX < widthB && yButton < mouseY && mouseY < heightB) {
-    console.log('click');
     screenChanger(screen);
   }
 }
 
-
 function menu() {
   chat();
-  solicitudesEnCurso();
-  solicitudesFinalizadas();
+  ongoingRequests();
+  completedRequest();
 }
 
 function chat() {
-  button({xButton: 51, yButton: 853, wButton: 27, hButton: 73, screen: 0});
+  button({
+    xButton: 51,
+    yButton: 853,
+    wButton: 27,
+    hButton: 73,
+    screen: 0
+  });
 }
 
-function solicitudesEnCurso() {
-  button({xButton: 185, yButton: 853, wButton: 60, hButton: 73, screen: 4});
+function ongoingRequests() {
+  button({
+    xButton: 185,
+    yButton: 853,
+    wButton: 60,
+    hButton: 73,
+    screen: 4
+  });
 }
 
-function solicitudesFinalizadas() {
-  button({xButton: 333, yButton: 853, wButton: 44, hButton: 73, screen: 5});
+function completedRequest() {
+  button({
+    xButton: 333,
+    yButton: 853,
+    wButton: 44,
+    hButton: 73,
+    screen: 5
+  });
+}
+
+function back(returnScreen) {
+  button({
+    xButton: 28,
+    yButton: 57,
+    wButton: 16,
+    hButton: 28,
+    screen: returnScreen
+  });
+}
+
+function flowRequest() {
+  switch (currentScreen) {
+    case 0:
+      button({
+        xButton: 283,
+        yButton: 235,
+        wButton: 115,
+        hButton: 35,
+        screen: 1
+      });
+
+      button({
+        xButton: 283,
+        yButton: 381,
+        wButton: 115,
+        hButton: 35,
+        screen: 1
+      });
+
+      button({
+        xButton: 283,
+        yButton: 527,
+        wButton: 115,
+        hButton: 35,
+        screen: 1
+      });
+
+      button({
+        xButton: 283,
+        yButton: 673,
+        wButton: 115,
+        hButton: 35,
+        screen: 1
+      });
+
+      break;
+    case 1:
+      back(0);
+      button({
+        xButton: 55,
+        yButton: 752,
+        wButton: 146,
+        hButton: 52,
+        screen: 2
+      });
+      button({
+        xButton: 226,
+        yButton: 752,
+        wButton: 146,
+        hButton: 52,
+        screen: 0
+      });
+      break;
+    case 2:
+      back(1);
+      button({
+        xButton: 111,
+        yButton: 664,
+        wButton: 205,
+        hButton: 54,
+        screen: 1
+      });
+      break;
+    case 3:
+      back(1);
+      button({
+        xButton: 50,
+        yButton: 540,
+        wButton: 146,
+        hButton: 52,
+        screen: 0
+      });
+
+      button({
+        xButton: 221,
+        yButton: 540,
+        wButton: 146,
+        hButton: 52,
+        screen: 1
+      });
+
+      break;
+  }
+}
+
+function loadingRequest() {
+  if (currentScreen === 2) {
+    setTimeout(() => {
+      currentScreen = 3;
+    }, 3000)
+  }
 }
